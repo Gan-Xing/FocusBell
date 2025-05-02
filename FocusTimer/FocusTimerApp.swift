c6cf8608e2ebbe2eff07c79ada5049f0f8228d4b
ganxing <599153574@qq.com>
2025-05-02 14:15:03 +0000

    Initial Commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by 甘星 on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusTimerTests/FocusTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerTests.swift
+//  FocusTimerTests
+//
+//  Created by 甘星 on 02/05/2025.
+//
+
+import Testing
+@testable import FocusTimer
+
+struct FocusTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
